383be4110c22cb43961475a345625e7747e85d24
Todd Whitelow <todd@whitelow.tech>
2021-09-16 22:53:12 -0600

    create quote model

---
+++ db/migrate/20210917045233_create_quotes.rb
@@ -0,0 +1,32 @@
+class CreateQuotes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :quotes do |t|
+      t.string :status
+      t.integer :price
+      t.string :client_name
+      t.string :email
+      t.string :phone
+      t.string :project_type
+      t.string :floors
+      t.int :building_sqft
+      t.boolean :garage
+      t.string :roof_material
+      t.string :pitch_of_roof
+      t.string :gutter_sqft
+      t.string :ext_material
+      t.boolean :screen_removal
+      t.string :door_material
+      t.int :flt_surface_sqft
+      t.string :driveway_size
+      t.string :comments
+      t.boolean :roof_services
+      t.boolean :gutter_cleaning
+      t.boolean :building_wash
+      t.boolean :flat_surfaces
+      t.boolean :rust_removal
+      t.boolean :driveway
+
+      t.timestamps
+    end
+  end
+end
