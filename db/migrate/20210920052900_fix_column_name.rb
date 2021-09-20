fb39f4ed6064b43bf64d5928ff2089b311802b4f
Todd Whitelow <todd@whitelow.tech>
2021-09-19 23:42:31 -0600

    add end to change table segment

--- db/migrate/20210920052900_fix_column_name.rb
+++ db/migrate/20210920052900_fix_column_name.rb
@@ -2,5 +2,6 @@ class FixColumnName < ActiveRecord::Migration[6.1]
   def change
     change_table :offered_services do |t|
       t.rename :img_url, :image
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_20_012526) do
+ActiveRecord::Schema.define(version: 2021_09_20_052900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2021_09_20_012526) do
   create_table "offered_services", force: :cascade do |t|
     t.string "name"
     t.string "description"
-    t.string "img_url"
+    t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
