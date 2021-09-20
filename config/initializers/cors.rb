b8228510a8335fed8c6f26a30c81c8fac5343d01
Todd Whitelow <todd@whitelow.tech>
2021-09-19 21:00:36 -0600

    add www to cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'ocalapressurewashing.co', 'example.com'
+    origins 'www.ocalapressurewashing.co', 'example.com'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
