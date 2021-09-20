a7855f31c9c5e022bd291aa1c26bfa8a8fd29b79
Todd Whitelow <todd@whitelow.tech>
2021-09-19 19:41:30 -0600

    add sessions controller

--- Gemfile
+++ Gemfile
@@ -43,3 +43,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'http'
 gem 'rack-cors'
 gem 'jb'
+gem 'jwt'
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,7 @@ Rails.application.routes.draw do
 
     post "/users" => "users#create"
 
+    post "/sessions" => "sessions#create"
+
   end
 end
