7679f9f5ca82c07682742a27f14ed935b668a591
Todd Whitelow <todd@whitelow.tech>
2022-08-20 01:32:33 -0600

    fixed price validation

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,7 +3,7 @@ class Quote < ApplicationRecord
   has_many :picked_services
 
   validates_presence_of :client_name, :price, :email, :status, :project_type, :floors, :building_sqft, :garage
-  validates :price, numericality:  { greater_than: 0 }
+  validates :price, numericality:  { greater_than: -1 }
   validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
   validates :email, length: { minimum: 4, maximum: 254 } 
   validates_length_of :client_name , minimum: 4, maximum: 50
